7545667ffca1d441a3f8f2d172a810d270594615
Chris Ribble <chris.ribble@resi.io>
2026-04-14 10:57:31 -0500

    Prefer 4-byte pixel formats, drop 3-byte RGB (#9)
    PixelFormat changes:
    - Add RGB32 (default)
    - Add BGR32
    - Rename BGR -> BGR24
    - Rename GRAY -> GRAY8
    - Remove RGB
    
    Other changes:
    - Encourage callers to use RGB32 by making it the default
    - Remove PixelFormatConverter (was only used by PixelFormat.RGB)
    - Remove unused bytesPerPixel field
    
    Real-world applications that need to process pixel data can do so faster
    with BufferedImage.TYPE_INT_RGB, as the underlying DataBufferInt int[]
    can be read with fewer instructions (one per pixel) than a
    DataByteBuffer's byte[] (three per pixel).

--- example/src/main/java/io/github/chrisribble/ffmpeg/example/PixelFormat.java
+++ example/src/main/java/io/github/chrisribble/ffmpeg/example/PixelFormat.java
@@ -1,28 +1,24 @@
 package io.github.chrisribble.ffmpeg.example;
 
+import static io.github.chrisribble.ffmpeg8.FFmpeg.AV_PIX_FMT_0BGR;
+import static io.github.chrisribble.ffmpeg8.FFmpeg.AV_PIX_FMT_0RGB;
 import static io.github.chrisribble.ffmpeg8.FFmpeg.AV_PIX_FMT_BGR24;
 import static io.github.chrisribble.ffmpeg8.FFmpeg.AV_PIX_FMT_GRAY8;
-import static io.github.chrisribble.ffmpeg8.FFmpeg.AV_PIX_FMT_RGB24;
 
 import java.awt.image.BufferedImage;
 
 public enum PixelFormat {
-	RGB(3, AV_PIX_FMT_RGB24(), BufferedImage.TYPE_3BYTE_BGR),
-	BGR(3, AV_PIX_FMT_BGR24(), BufferedImage.TYPE_3BYTE_BGR),
-	GRAY(1, AV_PIX_FMT_GRAY8(), BufferedImage.TYPE_BYTE_GRAY);
+	RGB32(AV_PIX_FMT_0RGB(), BufferedImage.TYPE_INT_RGB),
+	BGR32(AV_PIX_FMT_0BGR(), BufferedImage.TYPE_INT_BGR),
+	BGR24(AV_PIX_FMT_BGR24(), BufferedImage.TYPE_3BYTE_BGR),
+	GRAY8(AV_PIX_FMT_GRAY8(), BufferedImage.TYPE_BYTE_GRAY);
 
-	private final int bytesPerPixel;
 	private final int ffmpegType;
 	private final int bufferedImageType;
 
-	PixelFormat(final int bytesPerPixel, final int ffmpegType, final int bufferedImageType) {
-		this.bytesPerPixel = bytesPerPixel;
+	PixelFormat(final int ffmpegType, final int bufferedImageType) {
 		this.ffmpegType = ffmpegType;
 		this.bufferedImageType = bufferedImageType;
-	}
-
-	public int bytesPerPixel() {
-		return bytesPerPixel;
 	}
 
 	public int ffmpegType() {
